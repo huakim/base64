13a3d77a7bcfd0dc44da37e4babb4bc17c6d37a3
Fux <109365408+Fuxfantx@users.noreply.github.com>
2023-07-24 01:36:02 +0800

    Bugfix about custom decoder; Use ipairs() for better performance in LuaJIT

--- base64.lua
+++ base64.lua
@@ -1,3 +1,4 @@
+-- Source: https://github.com/iskolbin/lbase64
 --[[
 
  base64 -- v1.5.3 public domain Lua base64 encoder/decoder
@@ -24,60 +25,56 @@
 
 local base64 = {}
 
-local extract = _G.bit32 and _G.bit32.extract -- Lua 5.2/Lua 5.3 in compatibility mode
-if not extract then
-	if _G.bit then -- LuaJIT
-		local shl, shr, band = _G.bit.lshift, _G.bit.rshift, _G.bit.band
-		extract = function( v, from, width )
-			return band( shr( v, from ), shl( 1, width ) - 1 )
-		end
-	elseif _G._VERSION == "Lua 5.1" then
-		extract = function( v, from, width )
-			local w = 0
-			local flag = 2^from
-			for i = 0, width-1 do
-				local flag2 = flag + flag
-				if v % flag2 >= flag then
-					w = w + 2^i
-				end
-				flag = flag2
-			end
-			return w
-		end
-	else -- Lua 5.3+
-		extract = load[[return function( v, from, width )
-			return ( v >> from ) & ((1 << width) - 1)
-		end]]()
-	end
+local shl = bit.lshift
+local shr = bit.rshift
+local band = bit.band
+
+local sub = string.sub
+local rep = string.rep
+local char = string.char
+local byte = string.byte
+local char = string.char
+local gsub = string.gsub
+local format = string.format
+
+local concat = table.concat
+local b64t = {[0]='A','B','C','D','E','F','G','H','I','J',
+'K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y',
+'Z','a','b','c','d','e','f','g','h','i','j','k','l','m','n',
+'o','p','q','r','s','t','u','v','w','x','y','z','0','1','2',
+'3','4','5','6','7','8','9','+','/','='}
+
+local function extract(v,from,width)
+	return band( shr( v, from ), shl( 1, width ) - 1 )
 end
 
-
-function base64.makeencoder( s62, s63, spad )
+local function makeencoder( s62, s63, spad )
 	local encoder = {}
-	for b64code, char in pairs{[0]='A','B','C','D','E','F','G','H','I','J',
-		'K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y',
-		'Z','a','b','c','d','e','f','g','h','i','j','k','l','m','n',
-		'o','p','q','r','s','t','u','v','w','x','y','z','0','1','2',
-		'3','4','5','6','7','8','9',s62 or '+',s63 or'/',spad or'='} do
+	b64t[62] = s62 or '+'
+	b64t[63] = s63 or '/'
+	b64t[64] = s64 or '='
+	encoder[0] = byte(b64t[0])
+	for b64code, char in ipairs(b64t) do
 		encoder[b64code] = char:byte()
 	end
 	return encoder
 end
 
-function base64.makedecoder( s62, s63, spad )
+local function makedecoder( s62, s63, spad )
 	local decoder = {}
-	for b64code, charcode in pairs( base64.makeencoder( s62, s63, spad )) do
+	local encode_source = makeencoder(s62, s63, spad)
+	decoder[ encode_source[0] ] = 0
+	for b64code, charcode in ipairs(encode_source) do
 		decoder[charcode] = b64code
 	end
 	return decoder
 end
 
-local DEFAULT_ENCODER = base64.makeencoder()
-local DEFAULT_DECODER = base64.makedecoder()
+local DEFAULT_ENCODER = makeencoder()
+local DEFAULT_DECODER = makedecoder()
 
-local char, concat = string.char, table.concat
 
-function base64.encode( str, encoder, usecaching )
+local function encode( str, encoder, usecaching )
 	encoder = encoder or DEFAULT_ENCODER
 	local t, k, n = {}, 1, #str
 	local lastn = n % 3
@@ -109,7 +106,9 @@ function base64.encode( str, encoder, usecaching )
 	return concat( t )
 end
 
-function base64.decode( b64, decoder, usecaching )
+local function decode( b64, decoder, usecaching )
+	-- Contains Bugfix from the Issue https://github.com/iskolbin/lbase64/issues/5
+	
 	decoder = decoder or DEFAULT_DECODER
 	local pattern = '[^%w%+%/%=]'
 	if decoder then
@@ -117,15 +116,16 @@ function base64.decode( b64, decoder, usecaching )
 		for charcode, b64code in pairs( decoder ) do
 			if b64code == 62 then s62 = charcode
 			elseif b64code == 63 then s63 = charcode
+			elseif b64code == 64 then spad = charcode
 			end
 		end
-		pattern = ('[^%%w%%%s%%%s%%=]'):format( char(s62), char(s63) )
+		pattern = ('[^%%w%%%s%%%s%%%s]'):format( char(s62), char(s63), char(spad) )
 	end
 	b64 = b64:gsub( pattern, '' )
 	local cache = usecaching and {}
 	local t, k = {}, 1
 	local n = #b64
-	local padding = b64:sub(-2) == '==' and 2 or b64:sub(-1) == '=' and 1 or 0
+	local padding = b64:sub(-2) == char(spad):rep(2) and 2 or b64:sub(-1) == char(spad) and 1 or 0
 	for i = 1, padding > 0 and n-4 or n, 4 do
 		local a, b, c, d = b64:byte( i, i+3 )
 		local s
@@ -156,7 +156,12 @@ function base64.decode( b64, decoder, usecaching )
 	return concat( t )
 end
 
+base64.makeencoder = makeencoder
+base64.makedecoder = makedecoder
+base64.encode = encode
+base64.decode = decode
 return base64
+
 
 --[[
 ------------------------------------------------------------------------------
